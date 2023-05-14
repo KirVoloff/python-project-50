3eb42118e262a7b87faaa904c90363ad72466e36
Vol'nov Kirill <volnov-93@mail.ru>
2023-05-14 15:53:19 +0300

    tests/test_parser.py

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -3,10 +3,10 @@ from gendiff.parser import parse
 
 def test_parse_module():
     json_file = parse(open('tests/fixtures/file1.json'), 'json')
-    yml_file = parse(open('tests/fixtures/file2.yml'), 'yml')
+    yaml_file = parse(open('tests/fixtures/file2.yml'), 'yaml')
     data_type = type(json_file)
     assert data_type == dict
     assert json_file == {'host': 'hexlet.io', 'timeout': 50,
                          'proxy': '123.234.53.22', 'follow': False}
-    assert yml_file == {'host': 'hexlet.io', 'timeout': 20,
+    assert yaml_file == {'host': 'hexlet.io', 'timeout': 20,
                         'verbose': True}
